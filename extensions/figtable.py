07e7c5b3d272442d32a62005980202bd94caf6dd
Matthew Newville <newville@cars.uchicago.edu>
2017-08-23 17:22:07 -0500

    use Directive from docutils

--- extensions/figtable.py
+++ extensions/figtable.py
@@ -5,7 +5,7 @@ around a table.
 
 from docutils import nodes
 import docutils.parsers.rst.directives as directives
-from sphinx.util.compat import Directive
+from docutils.parsers.rst import Directive
 from sphinx import addnodes
 
 class figtable(nodes.General, nodes.Element):
